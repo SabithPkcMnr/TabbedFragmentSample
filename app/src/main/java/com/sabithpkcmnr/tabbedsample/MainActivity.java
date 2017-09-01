b392654e11595d2d2b0fcaf15a266c3cf821c267
Sabith Pkc Mnr <sabithpkcmnr@gmail.com>
2017-09-01 14:15:58 +0530

    Project by Sabith Pkc Mnr

---
+++ app/src/main/java/com/sabithpkcmnr/tabbedsample/MainActivity.java
@@ -0,0 +1,133 @@
+package com.sabithpkcmnr.tabbedsample;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    myFragOne myTab1 = new myFragOne();
+                    return myTab1;
+                case 1:
+                    myFragTwo myTab2 = new myFragTwo();
+                    return myTab2;
+                case 2:
+                    myFragThree myTab3 = new myFragThree();
+                    return myTab3;
+                case 3:
+                    myFragFourth myTab4 = new myFragFourth();
+                    return myTab4;
+                case 4:
+                    myFragFifth myTab5 = new myFragFifth();
+                    return myTab5;
+
+                default:
+                    return null;
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            // Show 5 total pages.
+            return 5;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "ONE";
+                case 1:
+                    return "TWO";
+                case 2:
+                    return "THREE";
+                case 3:
+                    return "FOUR";
+                case 4:
+                    return "FIVE";
+                case 5:
+                    return "SIX";
+            }
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/sabithpkcmnr/tabbedsample/myFragTwo.java
@@ -0,0 +1,27 @@
+package com.sabithpkcmnr.tabbedsample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by SABITH on 9/1/2017.
+ */
+
+public class myFragTwo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.frag_two, container, false);
+        return myView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
